9a27cc9eb0101c2245153b1903b050c26ab45ebf
Wallef <wallef.fa@gmail.com>
2018-05-09 16:16:23 -0300

    First commit.

---
+++ src/main/java/com/ais/oauth/EmployeeResource.java
@@ -0,0 +1,21 @@
+package com.ais.oauth;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ais.oauth.model.Employee;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeResource {
+	
+	@GetMapping(produces="application/json")
+	public Employee getEmployee() {
+		Employee e = new Employee();
+		e.setName("John");
+		e.setAge(27);
+		
+		return e;
+	}
+}
